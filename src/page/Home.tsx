f0e4b4e2ef52ada4d119806e670cb2f9fa5bddfa
Johan Groenen <johan.groenen@gmail.com>
2020-10-03 20:09:38 +0200

    react requests for unique key for siblings

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -45,9 +45,10 @@ export class HomePage extends React.Component {
 
     colorNote = (text: string) => {
         const textArray = text.split(' ')
+        var i = 0;
         const coloredText = textArray.map(text => {
             if ((/\B(#[a-zA-Z]+\b)(?!;)/).test(text)) {
-              return <span className="note-tag">{text}</span>;
+              return <span key={i++} className="note-tag">{text}</span>;
             }
             return text + ' ';
           });
